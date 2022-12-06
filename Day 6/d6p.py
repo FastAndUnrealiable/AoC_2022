576bde7133010e6b1beaaa94df725a96bf8425e6
FastAndUnrealiable <92855097+FastAndUnrealiable@users.noreply.github.com>
2022-12-06 08:06:12 +0100

    Update d6p.py

--- Day 6/d6p.py
+++ Day 6/d6p.py
@@ -4,7 +4,7 @@ with open("F:\AoC_2022\Day 6\input.txt", "r") as sentence:
 
     for i in range(len(sentence.readline())):
         if len(set(linecache.getline("F:\AoC_2022\Day 6\input.txt", 1)[i-4:i])) == 4:
-            print(f"First marker after {i} characters.")
+            print(f"First marker found after {i} characters.")
             if len(set(linecache.getline("F:\AoC_2022\Day 6\input.txt", 1)[i-14:i])) == 14:
                 print(f"14 unique found after {i} characters.")
 
